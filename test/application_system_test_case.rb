ac3af599286572910b54417a2be218a45c05c357
kaiyu-tech <kaiyu326@gmail.com>
2021-11-10 00:34:53 +0900

    Browser指定の誤りを修正

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -6,7 +6,7 @@ Webdrivers::Chromedriver.update
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   if ENV['CI'] # True in GitHub Actions.
-    driven_by :selenium, using: :chrome_headless, screen_size: [1400, 1400]
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
   else
     driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
   end
