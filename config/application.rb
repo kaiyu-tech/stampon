4738ddcc76640a986a4a271095e600a111d2aa17
kaiyu-tech <kaiyu326@gmail.com>
2021-11-09 23:29:02 +0900

    initial setting

--- config/application.rb
+++ config/application.rb
@@ -25,11 +25,9 @@ module Stampon
     config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    config.generators.template_engine = :slim
   end
 end
