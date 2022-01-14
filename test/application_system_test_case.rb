f25861d825182fefa7dca9e3c8e8e90de796c77d
kaiyu-tech <kaiyu326@gmail.com>
2022-01-15 08:56:54 +0900

    テスト定数の修正

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -5,11 +5,10 @@ require 'test_helper'
 Webdrivers::Chromedriver.update
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  HEADLESS = ENV['HEADLESS'] || ENV['CI'] || false
+  VUEJS_TIME_OUT = (ENV['VUEJS_TIME_OUT'] || 3).to_i
 
-  HEADLESS = (ENV['HEADLESS'] || 1).to_i 
-  VUEJS_TIME_OUT = (ENV['VUEJS_TIME_OUT'] || 5).to_i
-
-  if HEADLESS
+  if HEADLESS 
     driven_by :selenium_chrome_headless, screen_size: [1400, 1400]
   else
     driven_by :selenium_chrome, screen_size: [1400, 1400]
